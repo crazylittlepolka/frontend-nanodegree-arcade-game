accd83fa68153519c2d4019bf59140a544b33f58
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-05-09 20:47:30 +0200

    Enemy moves with random speed

--- js/app.js
+++ js/app.js
@@ -12,10 +12,15 @@ var Enemy = function(x,y) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+
+const speed = [40, 50, 65];
+let randomSpeed = speed[Math.floor(Math.random()*speed.length)];
+
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += randomSpeed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
